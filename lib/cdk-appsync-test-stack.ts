634cacb3d9db2fcb87c055e766b18c8555c25d25
ufoo68 <mitarou.yuutarou@gmail.com>
2020-04-29 16:39:33 +0900

    Implement scheme

--- lib/cdk-appsync-test-stack.ts
+++ lib/cdk-appsync-test-stack.ts
@@ -1,9 +1,17 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import { GraphQLApi, CfnApiKey } from '@aws-cdk/aws-appsync'
 
 export class CdkAppsyncTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const api = new GraphQLApi(this, 'graphQlApi', {
+      name: 'test-api',
+      schemaDefinitionFile: './schema.graphql',
+    })
+    
+    new CfnApiKey(this, 'apiKey', {
+      apiId: api.apiId,
+    })
   }
 }
