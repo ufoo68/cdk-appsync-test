6f72a523bbd7a842915710f3e04b7d6f1bb9bd66
ufoo68 <mitarou.yuutarou@gmail.com>
2020-04-30 12:03:06 +0900

    Implemented resolver

--- lib/cdk-appsync-test-stack.ts
+++ lib/cdk-appsync-test-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core'
-import { GraphQLApi, CfnApiKey } from '@aws-cdk/aws-appsync'
+import { GraphQLApi, CfnApiKey, MappingTemplate, PrimaryKey, Values } from '@aws-cdk/aws-appsync'
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb'
 
 export class CdkAppsyncTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,9 +10,32 @@ export class CdkAppsyncTestStack extends cdk.Stack {
       name: 'test-api',
       schemaDefinitionFile: './schema.graphql',
     })
-    
+
     new CfnApiKey(this, 'apiKey', {
       apiId: api.apiId,
+    })
+
+    const itemTable = new Table(this, 'itemTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      }
+    })
+
+    const itemDS = api.addDynamoDbDataSource('Item', 'Item data source', itemTable)
+    itemDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'allItem',
+      requestMappingTemplate: MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultList(),
+    })
+    itemDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'addItem',
+      requestMappingTemplate: MappingTemplate.dynamoDbPutItem(
+          PrimaryKey.partition('id').auto(),
+          Values.projecting('item')),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
     })
   }
 }
